d98a500984694293112034e5c4a4fd4a2a4b94b9
Cody <wilcoyonkin@gmail.com>
2024-11-14 15:24:51 -0500

    fixed deployment issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from './assets/pages/Home/Home.js'
 import { Routes, Route, useNavigate} from 'react-router-dom'
 import Login from './assets/pages/Login/Login.js'
@@ -6,6 +6,7 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase.js'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import loading_gif from '../../images/loading-anime.gif';
 
 function App() {
   const [loading, setLoading] = useState(true);
