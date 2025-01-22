aecc24e3ad7b232f8e64b0d7c3b51e81933c26ff
W. Cody Yonkin <WilCoYonkin@gmail.com>
2025-01-22 16:41:27 -0500

    working on mobile opt for navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate} from 'react-router-dom'
 import './Navbar.css'
 // import search_icon from '../../assets/images/search-icon.svg'
 // import bell_icon from '../../assets/images/bell-icon.svg'
 import profile_icon from'../../assets/images/profile-icon.svg'
 import drop_icon from'../../assets/images/drop-down.svg'
+import hamburger_icon from '../../assets/images/hamburger-menu.svg'
 import { logout} from '../../firebase'
 
 
 function Navbar() {
   const navigate = useNavigate();
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
 
   return (
@@ -18,7 +21,7 @@ function Navbar() {
         <div className='navbar-logo'>
           <h1>Anime<strong className='pulse'>Pulse</strong></h1>
         </div>
-        <ul>
+        <ul className={menuOpen ? "open slide-bottom" : ""}>
           <li>
             <button
               onClick={(e) => {
@@ -50,6 +53,7 @@ function Navbar() {
             </button>
           </li>
         </ul>
+      
       </div>
       <div className="navbar-right">
         <div className="navbar-profile">
@@ -63,6 +67,8 @@ function Navbar() {
             alt="Downward arrow"
             className="dropdown-arrow"
           />
+          
+          
           <div className="dropdown slide-bottom">
             <p
               onClick={() => {
@@ -73,7 +79,14 @@ function Navbar() {
             </p>
           </div>
         </div>
+        <img
+            src={hamburger_icon}
+            alt="Hamburger Menu Icon"
+            className="hamburger-menu"
+            onClick={() => setMenuOpen(!menuOpen)}
+        />
       </div>
+     
     </div>
   );
 }
